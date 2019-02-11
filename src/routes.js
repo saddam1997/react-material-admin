eaa995bb5bd94ba56bad54dc4223bdcb460c76b7
Arihant <arihant@brsoftech.com>
2019-02-11 12:55:30 +0530

    asdf

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NotFoundPage from './containers/NotFoundPage.js';
 import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
 import FormPage from './containers/FormPage';
 import TablePage from './containers/TablePage';
 import Dashboard from './containers/DashboardPage';
@@ -10,6 +11,7 @@ import Dashboard from './containers/DashboardPage';
 export default (
   <Route>
     <Route path="login" component={LoginPage}/>
+    <Route path="register" component={RegisterPage}/>
     <Route path="/" component={App}>
       <IndexRoute component={Dashboard}/>
       <Route path="dashboard" component={Dashboard}/>
